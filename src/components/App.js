5ef4efe49b87a5955e3a80d30d575ab23636a5df
Shanmugavel Sundaramoorthy <Shanmugavel@users.noreply.github.com>
2017-10-15 20:52:11 -0400

    Add multiple reducers

--- src/components/App.js
+++ src/components/App.js
@@ -13,14 +13,14 @@ class App extends Component {
   }
 }
 
-function mapStateToProps(cal) {
+function mapStateToProps({food, calendar}) {
   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
   return {
     calendar : dayOrder.map( (day) => ({
       day , 
-      meals: Object.keys(cal[day]).reduce( (meals, meal) => {
-        meals[meal] = cal[day][meal] ? cal[day][meal] : null
+      meals: Object.keys(calendar[day]).reduce( (meals, meal) => {
+        meals[meal] = calendar[day][meal] ? food[ calendar[day][meal]] : null
         return meals;
       }, {})
     }))
