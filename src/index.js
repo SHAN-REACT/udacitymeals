375245b4bc3f2cb1921fcd6e1dfff665b142edef
Shanmugavel Sundaramoorthy <Shanmugavel@users.noreply.github.com>
2017-10-15 20:24:05 -0400

    Integrate with react-redux package.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import './index.css';
 import App from './components/App';
 import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+    ReactDOM.render(
+        <Provider store={store}> 
+            <App />
+        </Provider>,
+        document.getElementById('root')
+      );
+      registerServiceWorker();
